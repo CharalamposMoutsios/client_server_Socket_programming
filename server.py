f9cccdb619ac33c00d4de3260807c871cc9c2e4c
CharalamposMoutsios <Charalampos.Moutsios@yh.nackademin.se>
2023-05-03 11:50:37 +0200

    socket client and server

---
+++ server.py
@@ -0,0 +1,17 @@
+import socket
+
+HOST = 'localhost'  # The server's hostname or IP address
+PORT = 8888        # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print('Server listening on port', PORT)
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
